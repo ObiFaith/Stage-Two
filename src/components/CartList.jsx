e3a72319e8430c5048cdad321d06e30c2fb41926
ObiFaith <obifth@gmail.com>
2024-07-15 12:17:33 +0100

    Update CartList.jsx

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -7,9 +7,9 @@ const CartList = ({id, img, name, price, colors, isNew, available, qty}) => {
 
   return (
     <div>
-      <div className="lg:grid flex gap-5 lg:grid-cols-5 md:gap-10 lg:gap-20">
+      <div className="lg:grid flex max-sm:flex-col gap-5 lg:grid-cols-5 md:gap-10 lg:gap-20">
         <div className="relative lg:border border-[#BAE2E1] rounded-[32px] lg:p-4">
-          <img src={img} alt='prod img' />
+          <img src={img} alt='prod img' className='max-sm:w-full object-cover' />
           {isNew && <img width={52} className="absolute top-3 left-3" src={new_img} alt='new product'/>}
         </div>
         <div className="lg:col-span-4 max-lg:w-full max-sm:w-auto grid lg:grid-cols-4">
@@ -24,7 +24,7 @@ const CartList = ({id, img, name, price, colors, isNew, available, qty}) => {
               </div>
               <ProdColors colors={colors} className='max-lg:hidden' />
             </div>
-            <div className="flex items-center max-sm:justify-center max-md:my-2 gap-4 sm:gap-6 lg:gap-x-10 border border-green-600 py-1 px-2.5 rounded-[20px]">
+            <div className="flex items-center max-sm:my-4 max-sm:justify-between max-md:my-2 gap-4 sm:gap-6 lg:gap-x-10 border border-green-600 py-1 px-2.5 rounded-[20px]">
               <div className='cursor-pointer'><img onClick={() => dispatch({type: 'DECR_PROD_QTY_IN_CART', payload: id})} src={minus} alt="sub" /></div>
               <span className='text-lg font-bold text-red-400'>{qty}</span>
               <div className='cursor-pointer'><img onClick={() => dispatch({type: 'INCR_PROD_QTY_IN_CART', payload: id})} src={plus} alt="add" /></div>
@@ -33,10 +33,10 @@ const CartList = ({id, img, name, price, colors, isNew, available, qty}) => {
           <div className="flex lg:*:ms-20 justify-between lg:justify-around col-span-2 items-center">
             <div className="flex gap-2">
               <ProdColors colors={colors} className='lg:hidden' />
-              <h3 className='lg:text-xl text-lg text-green-500'>${price}</h3>
+              <h3 className='max-lg:hidden lg:text-xl text-lg text-green-500'>${price}</h3>
             </div>
             <div className="flex justify-between items-center gap-4">
-              <h3 className='max-lg:hidden lg:text-xl text-lg text-green-500'>${price * qty}</h3>
+              <h3 className='lg:text-xl text-lg text-green-500'>${price * qty}</h3>
               <div className='cursor-pointer'><img onClick={() => dispatch({type: 'DEL_FROM_CART', payload: id})} src={del} alt="del" /></div>
             </div>
           </div>
